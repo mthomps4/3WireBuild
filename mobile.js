0fae5fa37cacb679a81ebe42ad6e5df49715516c
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-06-07 16:24:40 -0400

    tweaked CSS and mobile.js file for OnLoad function. Added first media break to files.

--- mobile.js
+++ mobile.js
@@ -1,6 +1,34 @@
 var button = document.getElementById('menuButton');
+var target = document.getElementById('main-nav');
+
+//Makes sure appropriate menu is loaded "onload"
+window.onload = function(){
+  if(window.innerWidth < 489){
+    target.style.display = "none";
+    button.style.display = "block";
+  }
+
+  if(window.innerWidth > 489){
+    button.style.display = "none";
+    target.style.display = "flex";
+    }
+}
+
+//Swaps Mobile Menu with Desktop Menu on Resize
+window.onresize = function(){
+    if(window.innerWidth < 489){
+      target.style.display = "none";
+      button.style.display = "block";
+    }
+
+    if(window.innerWidth > 489){
+      button.style.display = "none";
+      target.style.display = "flex";
+      }
+}
+
+//Mobile Menu Button Toggle 
 button.addEventListener('click', function(event) {
-    var target = document.getElementById('main-nav');
     if (target.style.display == "none"){
         target.style.display = "flex";
     } else {
