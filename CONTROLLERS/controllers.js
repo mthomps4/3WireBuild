423d144337e2f68af21584c6c73e581f8d77cfeb
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-06-28 15:54:03 -0400

    Testing problem with Window Onload function in Angular.

--- CONTROLLERS/controllers.js
+++ CONTROLLERS/controllers.js
@@ -17,3 +17,28 @@ app.controller('ResourcesController', function($scope) {
 app.controller('BlogController', function($scope) {
 
 });
+
+app.controller('NavController', function($scope,$window,$document){
+
+  $window.onload = function(){
+    var button = document.getElementById('menuButton');
+    var nav1 = document.getElementById('main-nav');
+    var nav2 = document.getElementById('main-nav2');
+
+    if(window.innerWidth < 489){
+      // alert("Window < 489");
+      nav1.style.display = "none";
+      button.style.display = "block";
+      nav2.style.display = "none";
+    }
+
+    if(window.innerWidth > 489){
+      // alert("Window > 489");
+      button.style.display = "none";
+      nav1.style.display = "none";
+      nav2.style.display = "flex";
+      }
+
+};
+
+});
--- app.js
+++ app.js
@@ -26,3 +26,34 @@ app.config(function($routeProvider){
   })
   .otherwise({redirectTo: '/'});
 });
+
+// app.run(['$window', function($window){
+//
+//   $window.onload = function(){
+//     var button = document.getElementById('menuButton');
+//     var nav1 = document.getElementById('main-nav');
+//     var nav2 = document.getElementById('main-nav2');
+//
+//     if(window.innerWidth < 489){
+//       alert("Window < 489");
+//       nav1.style.display = "none";
+//       button.style.display = "block";
+//       nav2.style.display = "none";
+//     }
+//
+//     if(window.innerWidth > 489){
+//       alert("Window > 489");
+//       button.style.display = "none";
+//       nav1.style.display = "none";
+//       nav2.style.display = "flex";
+//       }
+//   };
+// }]);
+
+//
+// Calling Order
+// app.config()
+// app.run()
+// directive's compile functions (if they are found in the dom)
+// app.controller()
+// directive's link functions (again, if found)
