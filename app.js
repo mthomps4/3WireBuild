6b271a1d250ecd86607df3badaab561bf7a29b7e
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-07-18 13:48:08 -0400

    prep for new branch

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = angular.module('app',["ngRoute"]);
 app.config(function($routeProvider){
   $routeProvider
   .when('/', {
-    templateUrl: 'TEMPLATES/_about.html',
+    templateUrl: 'TEMPLATES/_index.html',
     controller: 'AboutController'
   })
   .when('/about', {
